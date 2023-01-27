beb0b52bfcbed02b9c6bdb10ce339e9871bd7b48
Mohacel <118649370+mohacelhosen@users.noreply.github.com>
2023-01-27 09:15:19 +0600

    Update RopeCutting.java

--- Recursion/RopeCutting.java
+++ Recursion/RopeCutting.java
@@ -1,20 +1,20 @@
 package Recursion;
-
 public class RopeCutting {
     public static void main(String[] args) {
-        int n = 5;
-        int a = 3;
-        int b = 2;
-        int c = 1;
-        System.out.println(maxPiece(n,a,b,c));
+        int n = 10;
+        int a = 2;
+        int b = 5;
+        int c = 7;
+        int maxPieces = maxPiece(n, a, b, c);
+        
+        System.out.println(maxPieces);
 
     }
-    public static int maxPiece(int n, int a, int b, int c){
-        if (n==0) return 0;
-        if (n<0) return -1;
-        int tempMax = Math.max(maxPiece(n-a,a,b,c), maxPiece(n-b,a,b,c));
-        int finalMax = Math.max(tempMax, maxPiece(n-c,a,b,c));
-//        int max = (maxPiece(n-a,a,b,c) > maxPiece(n-b,a,b,c)) ? ((maxPiece(n-a,a,b,c)>maxPiece(n-c,a,b,c))? (maxPiece(n-a,a,b,c): maxPiece(n-c,a,b,c))) : ((maxPiece(n-b,a,b,c)>maxPiece(n-c,a,b,c))? (maxPiece(n-b,a,b,c):maxPiece(n-c,a,b,c));
+    public static int maxPiece(int n, int a, int b, int c) {
+        if (n == 0) return 0;
+        if (n < 0) return -1;
+        int finalMax = Math.max(maxPiece(n-a, a, b, c),
+                Math.max(maxPiece(n-b, a, b, c), maxPiece(n-c, a, b, c)));
         return finalMax;
     }
 }
